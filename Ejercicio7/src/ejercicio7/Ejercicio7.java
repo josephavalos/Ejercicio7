5e27243e3bf98bb0f0a5084cb51418e9a95e5602
ASUS <ASUS@DESKTOP-1B6T4JK>
2020-09-20 15:54:18 -0600

    este es el main del ejercicio 7

--- Ejercicio7/src/ejercicio7/Ejercicio7.java
+++ Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -1,26 +1,13 @@
+
 package ejercicio7;
 
-import java.util.Scanner;
 
 public class Ejercicio7 {
 
+  
     public static void main(String[] args) {
-        Scanner teclado = new Scanner (System.in);
-       int num1,num2;
-       try{
-           System.out.println("Ingrese el primer numero: ");
-       num1 = teclado.nextInt();
-       System.out.println("Ingrese el segundo numero: ");
-       num2 = teclado.nextInt();
-       
-       while( num1 != num2)
-           if (num1 > num2)
-               num1 = num1 - num2;
-           else num2 = num2 - num1;
-       System.out.println("El MCD es: "+num1);
-       }catch (Exception e){
-            System.out.println("ERROR: Por favor introduce números enteros...");
-        }
+        Mcd obj = new Mcd();
+        Mcd.Maximo();
     }
     
 }
