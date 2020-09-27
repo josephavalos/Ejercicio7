88c19a973c424d971b8d636671ce3c69e01ab0ce
ASUS <ASUS@DESKTOP-1B6T4JK>
2020-09-27 17:55:44 -0600

    aqui eata llamando las dos literales

--- Ejercicio7/src/ejercicio7/Ejercicio7.java
+++ Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -1,13 +1,17 @@
 
 package ejercicio7;
 
+import ejer2.Factor;
+
 
 public class Ejercicio7 {
 
   
     public static void main(String[] args) {
         Mcd obj = new Mcd();
-        Mcd.Maximo();
+        Factor Fact = new Factor();
+        Factor.Factorizar();
+         Mcd.Maximo();
     }
     
 }
